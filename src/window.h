8d3259897428fcb133a8ed6b8264b7e97bcc13cc
xroah <396069583@qq.com>
2020-07-16 22:38:17 +0800

    update

--- src/window.h
+++ src/window.h
@@ -12,3 +12,20 @@ gboolean handle_context_menu(WebKitWebView *, WebKitContextMenu *, GdkEvent *, W
 GtkWidget* handle_web_view_create(WebKitWebView *, WebKitNavigationAction *, gpointer);
 void init_web_view_settings(WebKitWebView *);
 WebKitWebView* add_web_view(GtkBuilder *, WebKitWebView *);
+
+typedef struct titlebar {
+    GtkWidget *add_btn;
+} titlebar;
+
+typedef struct topbar {
+    GtkWidget *back_btn;
+    GtkWidget *forward_btn;
+    GtkWidget *refresh_btn;
+    GtkWidget *url_input;
+    GtkWidget *menu_btn;
+} topbar;
+
+typedef struct window {
+    titlebar titlebar;
+    topbar topbar;
+} window;
