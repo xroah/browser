a865d3eaa90f7c93ae1a83bab53ed9aed2c12aea
xroah <396069583@qq.com>
2020-08-22 10:13:27 +0800

    default size and min size

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
+#define MIN_WIDTH 1024
+#define MIN_HEIGHT 700
 
 void activate(GtkApplication *app, gpointer user_data)
 {
@@ -8,6 +10,10 @@ void activate(GtkApplication *app, gpointer user_data)
     GtkCssProvider *css_provider = gtk_css_provider_new();
     GtkBuilder *builder = gtk_builder_new_from_file("ui/title-bar.glade");
     GtkWidget *title_bar = (GtkWidget *)gtk_builder_get_object(builder, "title-bar");
+    GdkGeometry geo = {
+        .min_width = MIN_WIDTH,
+        .min_height = MIN_HEIGHT
+    };
 
     gtk_css_provider_load_from_path(css_provider, "css/title-bar.css", NULL);
     gtk_style_context_add_provider_for_screen(
@@ -15,9 +21,11 @@ void activate(GtkApplication *app, gpointer user_data)
         (GtkStyleProvider *)css_provider,
         GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
     );
-    gtk_window_set_titlebar((GtkWindow *)win, title_bar);
-    gtk_window_set_default_size((GtkWindow *)win, 1024, 600);
+    gtk_window_set_titlebar((GtkWindow *) win, title_bar);
+    gtk_window_set_icon_from_file((GtkWindow *) win, "./icons/logo.png", NULL);
     gtk_widget_show_all(win);
+    gtk_window_set_default_size((GtkWindow *) win, MIN_WIDTH, MIN_HEIGHT);
+    gtk_window_set_geometry_hints((GtkWindow *) win, NULL, &geo, GDK_HINT_MIN_SIZE);
 }
 
 int main (int argc, char *argv[])
