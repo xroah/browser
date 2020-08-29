834135d2cb876fa831e5e0429238070d58e1b6fc
xroah <396069583@qq.com>
2020-08-29 17:07:37 +0800

    search

--- title_bar.c
+++ title_bar.c
@@ -6,12 +6,18 @@ void window_destroy(GtkWidget *window, gpointer user_data)
     free(user_data);
 }
 
+handle_search(GtkWidget *btn, gpointer user_data)
+{
+    return TRUE;
+}
+
 TitleBar* title_bar_new(GtkWidget *window) {
     TitleBar *title_bar = malloc(sizeof(title_bar));
     GtkCssProvider *css_provider = gtk_css_provider_new();
     GtkBuilder *builder = gtk_builder_new_from_file("ui/title-bar.glade");
     title_bar->bar = (GtkWidget *)gtk_builder_get_object(builder, "title-bar");
     title_bar->search_input = (GtkWidget *)gtk_builder_get_object(builder, "search-input");
+    title_bar->search_btn = (GtkWidget *)gtk_builder_get_object(builder, "search-btn");
 
     gtk_css_provider_load_from_path(css_provider, "css/title-bar.css", NULL);
     gtk_style_context_add_provider_for_screen(
@@ -20,6 +26,7 @@ TitleBar* title_bar_new(GtkWidget *window) {
         GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
     );
     g_signal_connect(window, "destroy", G_CALLBACK(window_destroy), title_bar);
+    g_signal_connect(title_bar->search_btn, "activate-link", G_CALLBACK(handle_search), NULL);
 
     return title_bar;
 }
--- title_bar.h
+++ title_bar.h
@@ -7,9 +7,11 @@ typedef struct
 {
     GtkWidget *search_input;
     GtkWidget *bar;
+    GtkWidget *search_btn;
 } TitleBar;
 
 void window_destroy(GtkWidget *, gpointer);
 TitleBar* title_bar_new(GtkWidget *);
+gboolean handle_search(GtkWidget *, gpointer);
 
 #endif // TITLE_BAR_H_
