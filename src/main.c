732a7febba492d21e8976dc1db6ecc6fe7698f78
vm <vm@localhost.localdomain>
2021-02-05 15:32:26 +0800

    ncurses

--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
-#include <stdio.h>
+#include <ncurses.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[])
 {
-    printf("Hello world");
+    initscr();
+    printw("Hello world! \n");
+    noecho();
+    cbreak();
+    keypad(stdscr, TRUE);
+
+    int c;
+
+    while(c = getch()) {
+        if (c == 'q') {
+            break;
+        }
+
+        clear();
+
+        switch(c) {
+            case KEY_UP:
+                printw("\nArrow up\n");
+                break;
+            case KEY_DOWN:
+                printw("\nArrow down\n");
+                break;
+            case KEY_LEFT:
+                printw("\nArrow left\n");
+                break;
+            case KEY_RIGHT:
+                printw("\nArrow right\n");
+                break;
+            default:
+                printw("\nOther keys\n");
+        }
+    }
+
+    endwin();
 
     return 0;
 }
